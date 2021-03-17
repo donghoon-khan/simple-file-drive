841eea1d4568175329d7754a94c1fe603bab452b
donghoon-khan <mustwin@kakao.com>
2021-03-17 21:32:10 +0900

    feature: front 추가

---
+++ frontend/src/layouts/DashboardLayout/NavBar/index.js
@@ -0,0 +1,143 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {
+  Box,
+  // Button,
+  Divider,
+  Drawer,
+  Hidden,
+  List,
+  // Typography,
+  makeStyles
+} from '@material-ui/core';
+import {
+  // AlertCircle as AlertCircleIcon,
+  BarChart as BarChartIcon,
+  Settings as SettingsIcon,
+  ShoppingBag as ShoppingBagIcon,
+  User as UserIcon,
+  Users as UsersIcon
+} from 'react-feather';
+import NavItem from './NavItem';
+
+const items = [
+  {
+    href: '/',
+    icon: BarChartIcon,
+    title: '내폴더'
+  },
+  {
+    href: '/',
+    icon: UsersIcon,
+    title: '공유폴더'
+  },
+  {
+    href: '/',
+    icon: ShoppingBagIcon,
+    title: '그룹폴더'
+  },
+  {
+    href: '/',
+    icon: UserIcon,
+    title: '메세지'
+  },
+  {
+    href: '/',
+    icon: SettingsIcon,
+    title: '백업'
+  },
+  // {
+  //   href: '/404',
+  //   icon: AlertCircleIcon,
+  //   title: 'Error'
+  // }
+];
+
+const useStyles = makeStyles(() => ({
+  mobileDrawer: {
+    width: 256
+  },
+  desktopDrawer: {
+    width: 256,
+    top: 64,
+    height: 'calc(100% - 64px)'
+  },
+  avatar: {
+    cursor: 'pointer',
+    width: 64,
+    height: 64
+  }
+}));
+
+const NavBar = ({ onMobileClose, openMobile }) => {
+  const classes = useStyles();
+  // const location = useLocation();
+
+  // useEffect(() => {
+  //   if (openMobile && onMobileClose) {
+  //     onMobileClose();
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [location.pathname]);
+
+  const content = (
+    <Box
+      height="100%"
+      display="flex"
+      flexDirection="column"
+    >
+      <Divider />
+      <Box p={2}>
+        <List>
+          {items.map((item) => (
+            <NavItem
+              href={item.href}
+              key={item.title}
+              title={item.title}
+              icon={item.icon}
+            />
+          ))}
+        </List>
+      </Box>
+    </Box>
+  );
+
+  return (
+    <>
+      <Hidden lgUp>
+        <Drawer
+          anchor="left"
+          classes={{ paper: classes.mobileDrawer }}
+          onClose={onMobileClose}
+          open={openMobile}
+          variant="temporary"
+        >
+          {content}
+        </Drawer>
+      </Hidden>
+      <Hidden mdDown>
+        <Drawer
+          anchor="left"
+          classes={{ paper: classes.desktopDrawer }}
+          open
+          variant="persistent"
+        >
+          {content}
+        </Drawer>
+      </Hidden>
+    </>
+  );
+};
+
+NavBar.propTypes = {
+  onMobileClose: PropTypes.func,
+  openMobile: PropTypes.bool
+};
+
+NavBar.defaultProps = {
+  onMobileClose: () => { },
+  openMobile: false
+};
+
+export default NavBar;
