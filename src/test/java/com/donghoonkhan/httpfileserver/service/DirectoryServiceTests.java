3fdbb3a9b376af5b2bb58eec86c1c60bacadebee
donghoon-khan <mustwin@kakao.com>
2021-03-24 19:53:30 +0900

    chore: Test 정리

--- src/test/java/com/donghoonkhan/httpfileserver/service/DirectoryServiceTests.java
+++ src/test/java/com/donghoonkhan/httpfileserver/service/DirectoryServiceTests.java
@@ -20,11 +20,13 @@ public class DirectoryServiceTests {
     @TempDir
     Path directory;
 
-    DirectoryService directoryService;
-
     @BeforeEach
     void setup() throws IOException {
-        directoryService = new DirectoryServiceImpl();
+    }
+
+    @Test
+    void Test_GetDirectory() throws IOException {
+        DirectoryService directoryService = new DirectoryServiceImpl();
         Path file1 = directory.resolve("file1.test");
         Path file2 = directory.resolve("file2.test");
         Path directory1 = directory.resolve("directory1");
@@ -34,32 +36,6 @@ public class DirectoryServiceTests {
         Files.createFile(file2);
         Files.createDirectory(directory1);
         Files.createDirectory(directory2);
-    }
-
-    @Test
-    void Test_GetDirectory() throws IOException {
         assertEquals(2, directoryService.getListDirectories(directory.toString()).size());
-    }
-
-    @Test
-    void Test_CreateDirectory() throws IOException {
-        final String NEW_DIRECTORY = directory.toString() + "/new";
-        assertDoesNotThrow(() -> directoryService.createDirectory(NEW_DIRECTORY));
-        try {
-            directoryService.createDirectory(NEW_DIRECTORY);
-        } catch(ResponseStatusException e) {
-            assertEquals(HttpStatus.BAD_REQUEST, e.getStatus());
-        }
-    }
-
-    @Test
-    void Test_DeleteDirectory() throws IOException {
-        final String DELETE_DIRECTORY = directory.toString() + "/directory1";
-        assertDoesNotThrow(() -> directoryService.deleteDirectory(DELETE_DIRECTORY));
-        try {
-            directoryService.deleteDirectory(DELETE_DIRECTORY);
-        } catch(ResponseStatusException e) {
-            assertEquals(HttpStatus.BAD_REQUEST, e.getStatus());
-        }
     }
 }
