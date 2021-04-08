07c2740aa2a7645f206481d10aa51dd8936c16f8
donghoon-khan <mustwin@kakao.com>
2021-04-08 21:32:52 +0900

    fix: not found symbol

--- src/main/java/com/donghoonkhan/simplefiledrive/config/Swagger2Config.java
+++ src/main/java/com/donghoonkhan/simplefiledrive/config/Swagger2Config.java
@@ -1,4 +1,4 @@
-package com.donghoonkhan.httpfileserver.config;
+package com.donghoonkhan.simplefiledrive.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
