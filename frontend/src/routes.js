fa51a270a04a43f08876b2f491d512d5571406a3
31 <a31@31ui-MacBookAir.local>
2021-04-22 21:09:11 +0900

    route 에러 수정

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,15 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import DashboardLayout from 'src/layouts/DashboardLayout';
 import MainLayout from 'src/layouts/MainLayout';
-
-
-import DashboardView from 'src/views/reports/DashboardView';
-
 import NotFoundView from 'src/views/errors/NotFoundView';
-
-
-
-import FileView from './views/files/Files';
 import ShareView from './views/share/ShareView/index.tsx';
 
 const routes = [
@@ -18,14 +10,7 @@ const routes = [
     path: 'app',
     element: <DashboardLayout />,
     children: [
-      
-      { path: 'files', element: <FileView /> },
       { path: 'share', element: <ShareView /> },
-      
-      { path: 'dashboard', element: <DashboardView /> },
-      
-      
-      
       { path: '*', element: <Navigate to="/404" /> }
     ]
   },
